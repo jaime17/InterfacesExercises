a327fbee741937f5d4b98296fa397af5a9cc20d2
jaime17 <A01411527@itesm.mx>
2017-03-15 17:54:46 -0600

    Create ResizableCircle.java

---
+++ ResizableCircle.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacesexercises;
+
+/**
+ *
+ * @author jaime
+ */
+public class ResizableCircle extends Circle implements Resizable {
+    
+
+    public ResizableCircle(double radius) {
+        super (radius);
+    }
+    
+    
+
+    @Override
+    public void resize(int porcent) {
+     this.radius*=(1+porcent)/100;
+    }
+    
+}
