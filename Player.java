66708aad5ee0c5b342f814cf703dc7af415556b1
jaime17 <A01411527@itesm.mx>
2017-03-15 17:53:11 -0600

    Create Player.java

---
+++ Player.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfacesexercises;
+
+/**
+ *
+ * @author jaime
+ */
+public interface Player {
+    public void play();
+    public void stop();
+    public void pause();
+    public void reverse();
+    
+    
+}
